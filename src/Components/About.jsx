229430a7a5746de93208b807fec893fbc02adda7
Noelia Paola Duran Llaveta <102623245+NoeliaDuranL@users.noreply.github.com>
2024-06-11 03:30:16 +0000

    datos agregados

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,21 +10,21 @@ import React from "react";
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/bird-about.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "a bird flying in the sky";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a university student studying at San Francisco Xavier de Chuquisaca University. I enjoy data management and creating web pages.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -32,11 +32,10 @@ const description =
  */
 const skillsList = [
   "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Data management",
+  "Basic programming",
+  "Web development",
+  "Networking",
 ];
 
 /**
@@ -45,7 +44,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I love data management and creating web pages. I have learned a little of everything in the 7 semesters I have been studying and I want to continue learning.";
 
 const About = () => {
   return (
